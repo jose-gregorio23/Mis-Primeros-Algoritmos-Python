73c2a140aa47035ff362b9c50f36b8a072335c9f
José Gregorio  Rodríguez Rincón <rodriguezgregoriojose003@gmail.com>
2026-04-04 20:28:28 -0500

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+# Mi primer programa para github 
+nombre = input("jose gregorio")
+print(f"¡Hola {nombre}! Bienvenido a mi repositorio de SENATI.")
+#Una logica simple
+ciclo_actual = 1
+total_ciclos = 6
+restantes = total de ciclos - ciclo_actual
+print(f"Actualmente estoy en el cilo {ciclo_actual}.")
+print(f"Me faltan {restantes} ciclos para ser ingeniero de Software.")
